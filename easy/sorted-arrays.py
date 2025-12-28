fc714e344fd8a9b6a98944199f70a304e7c4388e
tevoshw <tevoshw@gmail.com>
2025-12-28 13:32:59 -0300

    Remove the sorted arrays, cuz it's a linked node

--- easy/sorted-arrays.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        array = nums1 + nums2
-        print(array)
-        len_array = len(array)
-        print(len_array)
-
-solution = Solution()
-solution.findMedianSortedArrays([1,2,3], [1,5,6])
